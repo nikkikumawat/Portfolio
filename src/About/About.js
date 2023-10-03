6e0a3c9708d1cd5eeda6c8ce61b29c2cf6bbcd5e
nikkikumawat <nikkidigitalmarketer129@gmail.com>
2023-10-03 23:22:39 +0530

    update

--- src/About/About.js
+++ src/About/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 import selfImage from "../images/selfimage.jpg"
 import experience from "../images/istockphoto-1209636820-612x612.jpg"
 import education from "../images/Types-of-Education.jpg"
-import project from "../images/C:\Users\user\portfolio\front\src\images\projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.webp"
+import project from "../images/project.webp"
+
+
 import "./About.css"
 
 
@@ -27,7 +29,20 @@ function About() {
         <div className='Details'>
           <div className='experience'>
             <div className='image'>
+             <div className='img1'>
               <img src={experience}></img>
+              <h2>Experience</h2>
+              </div>
+              <div className='img1'>
+              <img src={education}></img>
+              <h2>Experience</h2>
+              </div>
+              <div className='img1'>
+              <img src={project}></img>
+              <h2>Experience</h2>
+              </div>
+              
+              
             </div>
           </div>
         </div>
