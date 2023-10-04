2bec9392d088655b1a473eac92741f19e61ea945
nikkikumawat <nikkidigitalmarketer129@gmail.com>
2023-10-04 23:49:12 +0530

    update

--- src/About/About.js
+++ src/About/About.js
@@ -13,36 +13,40 @@ function About() {
     <>
       <div className='About'>
         <div className='aboutpage'>
+
           <div className='selfimage'>
             <img src={selfImage}></img>
           </div>
           <div className='abouturself'>
             <h2>About</h2>
             <h3>Full Stack Developer</h3>
-            <p> a passionate and versatile full-stack  developer with a love for crafting web solutions that not only meet the needs of users but also deliver exceptional user experiences. I thrive on the challenges of turning complex ideas into functional and elegant digital products.</p>
+            <p> a passionate and versatile full-stack  developer with a love for crafting web solutions that not <br/>only meet the needs of users but also deliver exceptional user experiences. I thrive on the <br/>challenges of turning complex ideas into functional and elegant digital products.</p>
           </div>
-          {/* experiences image component detail
-          education image
-          project iamge  */}  
+
         </div>
+
 
         <div className='Details'>
           <div className='experience'>
             <div className='image'>
-             <div className='img1'>
-              <img src={experience}></img>
-              <h2>Experience</h2>
+              <div className='img1'>
+                <h2>Experience</h2>
+                <img src={experience}></img>
+                <a href=''>Click Here</a>
+
               </div>
               <div className='img1'>
-              <img src={education}></img>
-              <h2>Experience</h2>
+                <h2>Education</h2>
+                <img src={education}></img>
+                <a href=''>Click Here</a>
               </div>
+
               <div className='img1'>
-              <img src={project}></img>
-              <h2>Experience</h2>
+                <h2>Projects</h2>
+                <img src={project}></img>
+                <a href=''>Click Here</a>
               </div>
-              
-              
+
             </div>
           </div>
         </div>
